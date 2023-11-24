f15bfc1a4b3d98ad2cd161b94a66578ecda32110
Sardor1009 <islomovsardor1009@gmail.com>
2023-11-25 00:02:06 +0500

    pages added

---
+++ js/main.js
@@ -0,0 +1,41 @@
+function openNavbar() {
+    document.getElementById("navbar-responsive").style.left = "0";
+    // document.getElementById("navbar-responsive").style.top = "0";
+  }
+  function closeNavbar() {
+    document.getElementById("navbar-responsive").style.left = "calc(-100% - 40px)";
+    // document.getElementById("navbar-responsive").style.top = "-100%";
+  }
+  
+  document.getElementById("navbar-open").addEventListener("click", openNavbar);
+  document.getElementById("navbar-close").addEventListener("click", closeNavbar);
+
+
+
+  window.addEventListener("scroll", function () {
+    shrink();
+  });
+  
+  let navbar = document.getElementById("navbar");
+  
+  function shrink() {
+    if (scrollY > 100) {
+      navbar.classList.add("navbar-shrink");
+    } else {
+          navbar.classList.remove("navbar-shrink");
+      }
+  }
+
+
+
+
+  let modeBtn = document.getElementById("mode-btn");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "dark") {
+    this.firstElementChild.src = "images/light.svg";
+  } else {
+    this.firstElementChild.src = "images/dark.svg";
+  }
+  document.body.classList.toggle("dark");
+});
