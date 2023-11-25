0791c487dcd749f5c2688f542f6fccb91bdca441
Sardor1009 <islomovsardor1009@gmail.com>
2023-11-26 02:26:02 +0500

    first commit

--- js/main.js
+++ js/main.js
@@ -12,19 +12,7 @@ function openNavbar() {
 
 
 
-  window.addEventListener("scroll", function () {
-    shrink();
-  });
-  
-  let navbar = document.getElementById("navbar");
-  
-  function shrink() {
-    if (scrollY > 100) {
-      navbar.classList.add("navbar-shrink");
-    } else {
-          navbar.classList.remove("navbar-shrink");
-      }
-  }
+
 
 
 
@@ -38,4 +26,77 @@ modeBtn.addEventListener("click", function () {
     this.firstElementChild.src = "images/dark.svg";
   }
   document.body.classList.toggle("dark");
+});
+
+
+
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.classList.add('backtop-show')
+  } else {
+    backtop.classList.remove('backtop-show')
+  }
+}
+
+
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+		navbar.classList.remove("navbar-shrink");
+	}
+}
+
+
+
+
+
+
+
+const loading = document.getElementById("loading");
+
+const loadingDuration =10; // 2s
+
+setTimeout(() => {
+  loading.classList.add('loading-none');
+}, loadingDuration);
+
+
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    nav: true,
+    autoplay: false, 
+    autoplayHoverPause: 2000,
+    items: 2, 
+   
+    loop: true,
+    responsive: {
+      0: {
+        items: 1,
+        
+      },
+      1120: {
+        items: 2,
+      }
+    },
+    dots: true,
+    margin: 30,
+    navText: ['<img src="images/left.svg"/>', '<img src="images/rigth.svg"/>'],
+  });
 });
